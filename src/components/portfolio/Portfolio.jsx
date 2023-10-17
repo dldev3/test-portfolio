63569bd5d2fb0719fca2d660c26a690b60b4d445
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-10-17 20:33:09 +0530

    portfolio-dev-03

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,10 +9,29 @@ const items = [
         img: "https://images.pexels.com/photos/4968391/pexels-photo-4968391.jpeg?auto=compress&cs=tinysrgb&w=1600",
         desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
 
-    }
+    },
+    {
+        id: 2,
+        title: "React Commerce",
+        img: "https://images.pexels.com/photos/4968391/pexels-photo-4968391.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
+
+    },
+    {
+        id: 3,
+        title: "React Commerce",
+        img: "https://images.pexels.com/photos/4968391/pexels-photo-4968391.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
+
+    },
+    {
+        id: 4,
+        title: "React Commerce",
+        img: "https://images.pexels.com/photos/4968391/pexels-photo-4968391.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
+
+    },
 ];
-
-
 
 const Portfolio = () => {
     return (
