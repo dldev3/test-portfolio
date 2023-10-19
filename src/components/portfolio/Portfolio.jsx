00f05f9332deddd19edc52e5a86c6b4d1e675447
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-10-19 23:58:38 +0530

    portfolio --dev -06

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -31,6 +31,13 @@ const items = [
         desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
 
     },
+    {
+        id:5 ,
+        title: "Nuxtjs Commerce",
+        img: "https://images.pexels.com/photos/4968391/pexels-photo-4968391.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
+
+    },
 ];
 
 const Portfolio = () => {
