5e61417d439276af429cfb065d50637f2b127cd6
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-10-20 23:20:36 +0530

    animation done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
+import { useRef } from "react";
 import "./portfolio.scss";
-import { motion } from 'framer-motion';
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 
 const items = [
@@ -31,19 +32,38 @@ const items = [
         desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
 
     },
-    {
-        id:5 ,
-        title: "Nuxtjs Commerce",
-        img: "https://images.pexels.com/photos/4968391/pexels-photo-4968391.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Tenetur, error! Animi voluptas corporis voluptateme."
-
-    },
 ];
 
-const Portfolio = () => {
+const Single = ({ item }) => {
     return (
-        <div className='portfolio'>
-            Portfolio
+        <section>
+            {item.title}
+        </section>
+    )
+}
+
+const Portfolio = () => {
+
+    const ref = useRef();
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["end end", "start start"]
+    });
+
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30
+    });
+
+    return (
+        <div className='portfolio' ref={ref}>
+            <div className="progress">
+                <h1>Featured Works</h1>
+                <motion.div style={{ scaleX: scaleX }} className="progressBar"></motion.div>
+            </div>
+            {items.map((item) => (
+                <Single item={item} key={item.id} />
+            ))}
         </div>
     )
 }
